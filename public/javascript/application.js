305fe47477aca5266894a3f28baa24cd6aa49c81
Timothy Orbasido <t.orbasido@gmail.com>
2015-08-26 00:01:03 +0000

    edit button add

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,9 +1,9 @@
 $(function() {
   var statusDiv = $("#status");
+  var listBody = $("#contact-list-body");
 
   var handlers = {
     displayContacts: function(result){
-      var listBody = $("#contact-list-body");
 
       //clear list body to simulate reload
       listBody.empty();
@@ -16,7 +16,9 @@ $(function() {
         var email = $("<td>").text(contact.email);
         var deleteButton = $("<div>").addClass("btn").addClass("btn-danger")
                            .addClass("delete").attr("contact-id", contact.id).text("Delete");
-        var deleteCell = $("<td>").append(deleteButton);
+        var editButton = $("<div>").addClass("btn").addClass("btn-primary")
+                           .addClass("edit").attr("contact-id", contact.id).text("Edit");
+        var deleteCell = $("<td>").append(editButton).append(" ").append(deleteButton);
         var row = $("<tr>").append(firstname).append(lastname).append(email).append(deleteCell);
         listBody.append(row);
 
@@ -65,13 +67,53 @@ $(function() {
 
   handlers.loadContacts();
 
-  $("#contact-list-body").on("click", ".delete", function(){
+  listBody.on("click", ".delete", function(){
     var contactId = $(this).attr("contact-id");
     handlers.deleteContact(contactId);
   });
 
-  $("#create-form").on("submit", function(){
+  listBody.on("click", ".edit", function(){
+    var contactId = $(this).attr("contact-id");
     
+    var form = $("<form>").append(
+      // Creating Form Div and Adding <h2> and <p> Paragraph Tag in it.
+      $("<h3/>").text("Contact Form"), $("<p/>").text("This is my form. Please fill it out. It's awesome!"), $("<form/>", {
+      action: '#',
+      method: '#'
+      }).append(
+      // Create <form> Tag and Appending in HTML Div form1.
+      $("<input/>", {
+      type: 'text',
+      id: 'vname',
+      name: 'name',
+      placeholder: 'Your Name'
+      }), // Creating Input Element With Attribute.
+      $("<input/>", {
+      type: 'text',
+      id: 'vemail',
+      name: 'email',
+      placeholder: 'Your Email'
+      }), $("<textarea/>", {
+      rows: '5px',
+      cols: '27px',
+      type: 'text',
+      id: 'vmsg',
+      name: 'msg',
+      placeholder: 'Message'
+      }), $("<br/>"), $("<input/>", {
+      type: 'submit',
+      id: 'submit',
+      value: 'Submit'
+    })));
+
+    $(this).parent().append(form);
+
+
+
+  });
+
+  $("#contact-form").on("submit", function(){
+    alert($(this).attr("value"));
     var data = {
       firstname: $("#firstname").val(),
       lastname: $("#lastname").val(),
